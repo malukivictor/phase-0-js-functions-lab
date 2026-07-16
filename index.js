0147fd41f5d2b0106cb6cbe2f88684bc174691ba
Maluki Victor <victormaluki0@gmail.com>
2026-07-16 10:43:29 +0300

    functions lab

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return Math.max(num1, num2);
+}
+
+function isPalindrome(word) {
+  return word === word.split("").reverse().join("");
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discountAmount = originalPrice * (discountPercentage / 100);
+  return originalPrice - discountAmount;
+}
 
 
 
